caf0269e75cac094cc8c43a7584e45de10ea1321
shwetank00001 <mshwetank@gmail.com>
2024-12-19 16:02:10 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,35 @@ import React from 'react';
 import './index.css'
 
 function App(){
-    return(
-        <div className='mainContent'>
-            <h2>Fun Facts About React</h2>
-            <li>Good</li>
-            <li>Best</li>
-            <li>Nice</li>
-            <li>Awesome</li>
+
+    const fName = "Shwetank"
+    const lName = "Mishra"
+
+    const hours = new Date().getHours()
+    let time;
+
+    if( 0 < hours < 8 ){
+        time = "Early Morning";
+    }
+    else if (8 < hours < 12){
+        time = "Morning";
+    }
+    else if (12 < hours < 16){
+        time = "Noon";
+    }
+    else if (16 < hours < 20){
+        time = "Evening";
+    }
+
+    return (
+        <div>
+            <p>
+                Hello {fName} {lName}
+            </p>
+            <h3>It is currently {hours}</h3>
+            <p>Good {time} {fName}</p>
         </div>
-    )
+    );
 }
 
 export default App;
---
+++ src/Facts.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+function Facts() {
+    return (
+        <div className="mainContent">
+            <h2>Fun Facts About React</h2>
+            <li>Good</li>
+            <li>Best</li>
+            <li>Nice</li>
+            <li>Awesome</li>
+        </div>
+    );
+}
+
+export default Facts;

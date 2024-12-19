7b8cb3d5770eeff8cdd65b49ad767d2d498ceec0
shwetank00001 <mshwetank@gmail.com>
2024-12-19 15:53:09 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css'
+
+function App(){
+    return(
+        <div className='mainContent'>
+            <h2>Fun Facts About React</h2>
+            <li>Good</li>
+            <li>Best</li>
+            <li>Nice</li>
+            <li>Awesome</li>
+        </div>
+    )
+}
+
+export default App;
